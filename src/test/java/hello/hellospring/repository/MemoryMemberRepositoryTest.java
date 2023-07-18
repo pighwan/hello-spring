d36ce35a882f944a382d7aa48ae7d2c539fde507
pighwan21 <pighwan21@gmail.com>
2023-07-18 10:36:46 +0900

    섹션3. 회원관리 예제 - 백앤드 개발 부분 3) 회원 repository 테스트 케이스 작성

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,12 +1,18 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
-//import org.junit.jupiter.api.Assertions;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+// 회원 repository 테스트 케이스 작성
+// - 개발한 기능을 실행해서 테스트 할 때 자바의 main 메서드를 통해서 실행하거나, 웹 애플리케이션의 컨트롤러를 통해
+//   서 해당 기능을 실행한다. 이러한 방법은 준비하고 실행하는데 오래 걸리고, 반복 실행하기 어렵고 여러 테스트를 한번
+//   에 실행하기 어렵다는 단점이 있다. 자바는 JUint이라는 프레임워크로 테스트를 실행해서 이러한 문제를 해결한다.
 
 class MemoryMemberRepositoryTest {
 
@@ -28,8 +34,8 @@ class MemoryMemberRepositoryTest {
         Member result = repository.findById(member.getId()).get();
 //        System.out.println("result = "+(result == member));
 //        Assertions.assertEquals(member, result);
-//        Assertions.assertEquals(member, null);
-        Assertions.assertThat(member).isEqualTo(result);
+//        Assertions.assertEquals(member, null);        // Actual(실제값)이 null이라며 오류가 뜸.
+        assertThat(member).isEqualTo(result);
     }
 
     @Test
@@ -44,7 +50,7 @@ class MemoryMemberRepositoryTest {
 
         Member result = repository.findByName("spring1").get();
 
-        Assertions.assertThat(result).isEqualTo(member1);
+        assertThat(result).isEqualTo(member1);
     }
 
     @Test
@@ -59,7 +65,7 @@ class MemoryMemberRepositoryTest {
 
         List<Member> result = repository.findAll();
 
-        Assertions.assertThat(result.size()).isEqualTo(2);
+        assertThat(result.size()).isEqualTo(2);
     }
 
 
